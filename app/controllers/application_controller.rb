5d3174d534be72774439e2a59f2bd5c9d60b72b7
nao <example@example.com>
2012-09-10 09:50:45 +0900

    サイドバーコンテンツを別テンプレートに分離。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :load_sidebar_information
-
-  private
-  def load_sidebar_information
-    @sidebar_recent_entries = Entry.newly.limit(30)
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def recent_entries(limit=30)
+    Entry.newly.limit(limit)
+  end
 end
